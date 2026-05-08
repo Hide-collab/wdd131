f3958c3839ab36d8804710a0a73a7d23614e78bb
Hide Vhunzawabaya <vhunzawabayahide@gmail.com>
2026-05-08 12:55:02 +0200

    assignment

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// Current Year
+const year = new Date().getFullYear();
+document.getElementById("currentyear").textContent = year;
+
+// Last Modified
+document.getElementById("lastModified").textContent = document.lastModified;
